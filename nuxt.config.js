db0b64fdbd8c978842397280b0a3b733fd9a25a5
Pascalvdsteendesign <pascalvdsteen@hotmail.com>
2020-06-19 08:46:53 +0200

    include sass and test font

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,13 +17,15 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap' }
     ]
   },
   /*
   ** Global CSS
   */
   css: [
+    '~assets/scss/global.scss'
   ],
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
+      <h1 class="title red">
         pascalvdsteendesign
+        <p>test</p>
       </h1>
       <div class="links">
         <a
@@ -37,7 +38,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+
+.red {
+  color: red;
+
+  p {
+    color: green;
+  }
+
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -47,23 +59,6 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
 .subtitle {
   font-weight: 300;
